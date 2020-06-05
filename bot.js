551aade8b2962e16484ed9c8c7ef0e027d3f82ae
eyouale <51370727+eyouale@users.noreply.github.com>
2020-06-05 20:19:21 +0300

    Implementing Important MiddleWares

--- bot.js
+++ bot.js
@@ -2,4 +2,25 @@ const Telegraf = require('telegraf');
 
 const bot = new Telegraf('1175976203:AAFYTxArtWiAbbYcavwr2rBZD4fd-3svuzI');
 
+const helpMessage = `Say something to me 
+/start - start the bot
+/help - help
+/settings - settings`;
+// Start When The Use Invoke Or Select Start
+bot.start(ctx => {
+	ctx.reply('Hi I am Your Bot  ');
+	ctx.reply(helpMessage);
+});
+bot.help(ctx => {
+	ctx.reply('Is there a Problem');
+});
+bot.settings(ctx => {
+	ctx.reply('You have Entered setting Command');
+});
+bot.hears('hi', ctx => {
+	ctx.reply('Hi ' + ctx.from.first_name);
+});
+bot.hears('cat', ctx => {
+	ctx.reply('Meow ' + ctx.from.first_name);
+});
 bot.launch();
